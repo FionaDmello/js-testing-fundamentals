dcfa994ca704f215441603cb65eaee0a9e76ebda
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-03-19 16:10:42 +0100

    refactored code to abstract away assertions

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -1,2 +1,23 @@
+// the simplest form of automated testing has repeated code that can be abstracted away as shown below
+const { sum, subtract } = require("../math");
 
+let result;
 
+result = sum(3, 7);
+expect(result).toBe(10);
+
+result = subtract(7, 3);
+expect(result).toBe(4);
+
+function expect(actual) {
+  // returns an object of assertions
+  // each assertion can be accessed via the function name
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+    // i can keep adding other assertions like strict equality or greater than, lesser than comparisons etc
+  };
+}
