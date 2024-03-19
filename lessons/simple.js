df6a5a5fb1b93d72dd9a9843a5bed86ad4ba4c16
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-03-19 15:58:02 +0100

    created simplest form of tests for two math functions

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,17 @@
-const {sum, subtract} = require('../math')
+const sum = (a, b) => a - b;
+const subtract = (a, b) => a - b;
+let result, expected;
 
-let result, expected
-
-result = sum(3, 7)
-expected = 10
+// an automated test runs your code and throws an error when the expected result is not the same as the result output by the code run
+// following is the simplest form of tests to check the code above
+result = sum(3, 7);
+expected = 10;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 3)
-expected = 4
+result = subtract(7, 3);
+expected = 4;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
